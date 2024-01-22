2dbed6d11b8bbec7cc21997e7a74e239153ecbde
rendellsoberano <rendellsoberano@gmail.com>
2024-01-22 22:02:45 +0800

    updating api files

--- exercises.js
+++ exercises.js
@@ -7,85 +7,85 @@ const exe = {
             {
               name: "Hollow Hold",
               equipment: "None",
-              img: "../img/hallowholdworkout.jpg",
+              img: "img/abs_easy1.jpg",
             },
             {
               name: "2-Move Abs",
               equipment: "None",
-              img: "../img/abs_easy2.jpg",
+              img: "img/abs_easy2.jpg",
             },
             {
               name: "Selkie",
               equipment: "None",
-              img: "../img/abs_easy3.jpg",
+              img: "img/abs_easy3.jpg",
             },
           ],
           shoulder: [
             {
               name: "Battle Mage",
               equipment: "None",
-              img: "../img/shoulder_easy1.jpg",
+              img: "img/shoulder_easy1.jpg",
             },
             {
               name: "Archer",
               equipment: "None",
-              img: "../img/shoulder_easy2.jpg",
+              img: "img/shoulder_easy2.jpg",
             },
             {
               name: "Game On",
               equipment: "None",
-              img: "../img/shoulder_easy3.jpg",
+              img: "img/shoulder_easy3.jpg",
             },
           ],
           legs: [
             {
               name: "Core & Balance",
               equipment: "None",
-              img: "../img/legs_easy1.jpg",
+              img: "img/legs_easy1.jpg",
             },
             {
               name: "End Game",
               equipment: "None",
-              img: "../img/legs_easy2.jpg",
+              img: "img/legs_easy2.jpg",
             },
             {
               name: "Made With Love",
               equipment: "None",
-              img: "../img/legs_easy3.jpg",
+              img: "img/legs_easy3.jpg",
             },
           ],
           chest: [
             {
               name: "Everybody Loves Large Chest",
               equipment: "Dumbell",
-              img: "../img/chest_easy1.jpg",
+              img: "img/chest_easy1.jpg",
             },
             {
               name: "Office Push-ups",
               equipment: "Wall",
-              img: "../img/chest_easy2.jpg",
+              img: "img/chest_easy2.jpg",
             },
             {
               name: "Upper Body Tendon Strength",
               equipment: "None",
-              img: "../img/chest_easy3.jpg",
+              img: "img/chest_easy3.jpg",
             },
           ],
           back: [
             {
               name: "Back Work",
               equipment: "None",
-              img: "../img/back_easy1.jpg",
+              img: "img/back_easy1.jpg",
             },
             {
               name: "Fallback",
               equipment: "None",
-              img: "../img/back_easy2.jpg",
+              img: "img/back_easy2.jpg",
             },
             {
               name: "Pack A Punch",
               equipment: "None",
-              img: "../img/back_easy3.jpg",
+              img: "img/back_easy3.jpg",
             },
           ],
         },
@@ -96,85 +96,85 @@ const exe = {
             {
               name: "One Minute Plank",
               equipment: "None",
-              img: "../img/abs_med1.jpg",
+              img: "img/abs_med1.jpg",
             },
             {
               name: "Standing Core",
               equipment: "None",
-              img: "../img/abs_med2.jpg",
+              img: "img/abs_med2.jpg",
             },
             {
               name: "Military Abs",
               equipment: "None",
-              img: "../img/abs_med3.jpg",
+              img: "img/abs_med3.jpg",
             },
           ],
           shoulder: [
             {
               name: "Power 15",
               equipment: "None",
-              img: "../img/shoulder_med1.jpg",
+              img: "img/shoulder_med1.jpg",
             },
             {
               name: "Arms of Steel",
               equipment: "None",
-              img: "../img/shoulder_med2.jpg",
+              img: "img/shoulder_med2.jpg",
             },
             {
               name: "Swimmer",
               equipment: "None",
-              img: "../img/shoulder_med3.jpg",
+              img: "img/shoulder_med3.jpg",
             },
           ],
           legs: [
             {
               name: "Wild Cat",
               equipment: "None",
-              img: "../img/legs_med1.jpg",
+              img: "img/legs_med1.jpg",
             },
             {
               name: "High-volume Legs",
               equipment: "None",
-              img: "../img/legs_med2.jpg",
+              img: "img/legs_med2.jpg",
             },
             {
               name: "Together",
               equipment: "Jowa",
-              img: "../img/legs_med3.jpg",
+              img: "img/legs_med3.jpg",
             },
           ],
           chest: [
             {
               name: "Chest & Core",
               equipment: "Dumbell",
-              img: "../img/chest_med1.jpg",
+              img: "img/chest_med1.jpg",
             },
             {
               name: "Push, Pull Legs",
               equipment: "Dumbell",
-              img: "../img/chest_med1.jpg",
+              img: "img/chest_med2.jpg",
             },
             {
               name: "Azarinth Healer",
               equipment: "None",
-              img: "../img/chest_med1.jpg",
+              img: "img/chest_med3cd ...jpg",
             },
           ],
           back: [
             {
               name: "Threshold",
               equipment: "Dumbell",
-              img: "../img/back_med1.jpg",
+              img: "img/back_med1.jpg",
             },
             {
               name: "V-Taper",
               equipment: "Dumbell & Horizontal Bar",
-              img: "../img/back_med2.jpg",
+              img: "img/back_med2.jpg",
             },
             {
               name: "Tomboy",
-              equipment: "Dumbell & Horizontal Bar",
-              img: "../img/back_med3.jpg",
+              equipment: "Horizontal Bar",
+              img: "img/back_med3.jpg",
             },
           ],
         },
@@ -185,85 +185,85 @@ const exe = {
             {
               name: "Planche Prep",
               equipment: "None",
-              img: "../img/abs_hard1.jpg",
+              img: "img/abs_hard1.jpg",
             },
             {
               name: "Monster Core",
               equipment: "None",
-              img: "../img/abs_hard2.jpg",
+              img: "img/abs_hard2.jpg",
             },
             {
               name: "Extreme Abs",
               equipment: "None",
-              img: "../img/abs_hard3.jpg",
+              img: "img/abs_hard3.jpg",
             },
           ],
           shoulder: [
             {
               name: "One Arm Push-up Prep",
               equipment: "None",
-              img: "../img/shoulder_hard1.jpg",
+              img: "img/shoulder_hard1.jpg",
             },
             {
               name: "Defying Gravity",
               equipment: "None",
-              img: "../img/shoulder_hard2.jpg",
+              img: "img/shoulder_hard2.jpg",
             },
             {
               name: "The Smitter",
               equipment: "Dumbell",
-              img: "../img/shoulder_hard3.jpg",
+              img: "img/shoulder_hard3.jpg",
             },
           ],
           legs: [
             {
               name: "Piston Squat Prep",
               equipment: "None",
-              img: "../img/legs_hard1.jpg",
+              img: "img/legs_hard1.jpg",
             },
             {
               name: "Spring Board",
               equipment: "None",
-              img: "../img/legs_hard2.jpg",
+              img: "img/legs_hard2.jpg",
             },
             {
               name: "Squat & Co",
               equipment: "None",
-              img: "../img/legs_hard3.jpg",
+              img: "img/legs_hard3.jpg",
             },
           ],
           chest: [
             {
               name: "Chest, Arms & Core",
               equipment: "None",
-              img: "../img/chest_hard1.jpg",
+              img: "img/chest_hard1.jpg",
             },
             {
               name: "Buff",
               equipment: "Dumbell & Horizontal Bar",
-              img: "../img/chest_hard2.jpg",
+              img: "img/chest_hard2.jpg",
             },
             {
               name: "Front Lever Prep",
               equipment: "Horizontal Bar",
-              img: "../img/chest_hard3.jpg",
+              img: "img/chest_hard3.jpg",
             },
           ],
           back: [
             {
               name: "Push & Pull",
               equipment: "Horizontal Bar",
-              img: "../img/back_hard1.jpg",
+              img: "img/back_hard1.jpg",
             },
             {
               name: "Push-up Massacre",
               equipment: "None",
-              img: "../img/back_hard2.jpg",
+              img: "img/back_hard2.jpg",
             },
             {
               name: "Fighter's Club",
               equipment: "Horizontal Bar",
-              img: "../img/back_hard3.jpg",
+              img: "img/back_hard3.jpg",
             },
           ],
         },
